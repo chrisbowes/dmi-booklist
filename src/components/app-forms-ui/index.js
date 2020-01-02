31b988d4dc689ed84e6ca50ecf65dcdae8c1b0e0
Chris Bowes <chris.bowes@haufe.com>
2020-01-02 12:50:26 +0000

    Updates

--- src/components/app-forms-ui/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FormButton } from './app-forms-button.component';
-export { default as FormInput } from './app-forms-input.component';
-export { default as FormLabel } from './app-forms-label.component';
